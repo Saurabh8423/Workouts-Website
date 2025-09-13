ab02303250510d9031701e015f14f99b44f91f17
saurabh8423 <saurabhch9369@gmail.com>
2025-09-13 13:26:18 +0530

    Created useSingup authentication

--- client/src/Pages/Signup/Signup.js
+++ client/src/Pages/Signup/Signup.js
@@ -1,13 +1,19 @@
-import React, {useState} from "react";
-import "./SignupStyles.css"
+import React, { useState } from "react";
+import "./SignupStyles.css";
+import { useSignup } from "../../Hooks/useSignup"
 
 const Signup = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-   
-  const handleSubmit =(e) =>{
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { signup, error } = useSignup();
+
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await signup(email, password)
+    setEmail("")
+    setPassword("")
+    // console.log(email, password);
   }
   return (
     <div className="auth-container">
@@ -16,15 +22,16 @@ const Signup = () => {
         <form onSubmit={handleSubmit}>
           <div className="field">
             <label>Email:</label>
-            <input type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} value={email}  />
+            <input type="email" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} value={email} />
           </div>
 
           <div className="field">
             <label>Password:</label>
-            <input type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} value={password}  />
+            <input type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} value={password} />
           </div>
 
           <button type="submit">Login</button>
+          {error && <p>{error}</p>}
         </form>
         <p className="switch-auth">
           Don’t have an account? <a href="/signup">Sign up</a>
