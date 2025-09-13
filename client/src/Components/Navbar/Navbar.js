7e59e792d3be8cb77a30c9d9f0fe6d1fe665c410
saurabh8423 <saurabhch9369@gmail.com>
2025-09-13 17:23:54 +0530

    Created useLogout funtion and  authentication

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import "./NavbarStyle.css";
 import {Link} from "react-router-dom";
+import { useLogout } from '../../Hooks/useLogout';
+
 
 const Navbar = () => {
+  const {logout} = useLogout();
+
+  const handleClick =()=>{
+    logout();
+  }
   return (
     <nav>
         <Link to="/">
         <h1>WorkoutBuddy</h1>
         </Link>
+
+        <div>
+          <button onClick={handleClick}>Logout</button>
+        </div>
 
         <div className='auth'>
             <Link to="/signup">Sign Up</Link>
