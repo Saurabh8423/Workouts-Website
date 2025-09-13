36e8760725af67b13f0ecf48247844d140321e9f
saurabh8423 <saurabhch9369@gmail.com>
2025-09-14 00:14:35 +0530

    some change on Navbar

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarStyle.css";
 import { Link } from "react-router-dom";
 import { useLogout } from "../../Hooks/useLogout";
 import { useAuthContext } from "../../Hooks/useAuthContext";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
   const { user } = useAuthContext();
   const { logout } = useLogout();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleClick = () => {
     logout();
+    setMenuOpen(false); // close menu after logout
   };
+
   return (
-    <nav>
-      <Link to="/">
+    <nav className="navbar">
+      <Link to="/" className="logo">
         <h1>WorkoutBuddy</h1>
       </Link>
 
-      {user && (
-        <div className="logout">
-          <span>{user.email}</span>
-          <button onClick={handleClick}>Logout</button>
-        </div>
-      )}
+      {/* Hamburger toggle */}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </div>
 
-      {!user && (
-        <div className="auth">
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Login</Link>
-        </div>
-      )}
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        {user && (
+          <div className="logout">
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Logout</button>
+          </div>
+        )}
+
+        {!user && (
+          <div className="auth">
+            <Link to="/signup" onClick={() => setMenuOpen(false)}>
+              Sign Up
+            </Link>
+            <Link to="/login" onClick={() => setMenuOpen(false)}>
+              Login
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
